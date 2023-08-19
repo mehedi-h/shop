712976de261ccce58e2f7df4c62a03a5c073be65
mehedi-h <mehedihs2015@gmail.com>
2023-08-19 13:17:48 +0600

    modified code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function handleClickBtn(target) {
     const selectedFlowers = document.getElementById('selected-flowers');
     const itemName = (target.parentNode.childNodes[1].innerText);
     const li = document.createElement('li')
+
     li.innerText = itemName;
     selectedFlowers.appendChild(li)
 
@@ -13,7 +14,23 @@ function handleClickBtn(target) {
 
     total = parseInt(total) + parseInt(price);
 
-    document.getElementById('total-price').innerText = total;
+    const totalPriceString = document.getElementById('total-price');
+    const totalPrice = totalPriceString.innerText;
+    const totalAmount = parseInt(totalPrice);
+    totalPriceString.innerText = total;
+    // console.log(totalAmount)
 
-    // console.log(total)
+    const netPriceString = document.getElementById('net-price')
+    const netPrice = netPriceString.innerText;
+    const netPriceAmount = parseInt(netPrice);
+    console.log(netPriceAmount)
+    
+    const discountRate = totalAmount/100*30;
+    const netAmount = totalAmount-discountRate;
+    if (discountRate.length >= 1) {
+        netAmount += 1;
+    }
+    
+    netPriceString.innerText = netAmount;
 }
+
